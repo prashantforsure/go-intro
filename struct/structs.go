c097a6f91e6a3f8942b30859452251f8c31252e4
prashantforsure <prashantforsure@gmail.com>
2024-10-27 20:44:49 +0530

    struct

--- struct/structs.go
+++ struct/structs.go
@@ -8,6 +8,17 @@ type Person struct {
 	college string
 	phone   int
 }
+type Address struct {
+	city    string
+	state   string
+	country string
+}
+
+type Man struct {
+	name    string
+	gender  bool
+	address Address
+}
 
 func main() {
 	person1 := Person{
@@ -16,6 +27,16 @@ func main() {
 		college: "LNCT",
 		phone:   12345,
 	}
-	fmt.Printf("name: %s, age: %d, college: %s, phone: %d", person1.name, person1.age, person1.college, person1.phone)
+	man1 := Man{
+		name:   "adarsh",
+		gender: true,
+		address: Address{
+			city:    "New York",
+			state:   "NY",
+			country: "USA",
+		},
+	}
+	fmt.Printf("name: %s, age: %d, college: %s, phone: %d\n", person1.name, person1.age, person1.college, person1.phone)
+	fmt.Printf("name: %s, gender: %t, city: %s, state: %s", man1.name, man1.gender, man1.address.city, man1.address.state)
 
 }
